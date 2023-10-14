ce9a175b2be697173cdce39ea633abd451668d42
Lê Thái Sơn <lethaison04011999@gmail.com>
2023-10-14 23:53:24 +0700

    [add] product new and product best seliing

--- src/scenes/Product/components/SliderProduct/index.jsx
+++ src/scenes/Product/components/SliderProduct/index.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import Slider from 'react-slick';
 import ItemProduct from '../ItemProduct';
-function SampleNextArrow(props) {
-  const { className, style, onClick } = props;
-  return <div className={className} style={{ ...style, display: 'block', background: 'red' }} onClick={onClick} />;
-}
+import './style.scss';
+
 const SliderProduct = ({ listProduct }) => {
   var settings = {
     dots: false,
@@ -12,32 +10,27 @@ const SliderProduct = ({ listProduct }) => {
     speed: 500,
     autoplay: true,
     autoplaySpeed: 2000,
-    slidesToShow: 4,
+    slidesToShow: 5,
     slidesToScroll: 1,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SampleNextArrow />,
+
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          nextArrow: <SampleNextArrow />,
-          prevArrow: <SampleNextArrow />,
         },
       },
       {
         breakpoint: 768,
         settings: {
           slidesToShow: 1,
-          nextArrow: <SampleNextArrow />,
-          prevArrow: <SampleNextArrow />,
         },
       },
     ],
   };
   return (
     <div>
-      <div className="px-[20px] ">
+      <div className="mx-[24px] wrap-slider-product">
         <Slider {...settings}>
           {listProduct &&
             listProduct.length > 0 &&
